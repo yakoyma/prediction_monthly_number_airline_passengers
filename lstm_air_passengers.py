7278a17e2b8e2c4ba00229bbbf2b0086791f91e9
yakoyma <96658352+yakoyma@users.noreply.github.com>
2024-02-08 02:19:45 +0100

    Update of Data Analysis
    Signed-off-by: yakoyma <96658352+yakoyma@users.noreply.github.com>

--- lstm_air_passengers.py
+++ lstm_air_passengers.py
@@ -22,12 +22,10 @@ import numpy as np
 import matplotlib.pyplot as plt
 import pandas as pd
 import sklearn
-import ydata_profiling
-import sktime
 import tensorflow as tf
 import keras_tuner
+import sktime
 
-from ydata_profiling import ProfileReport
 from sklearn.preprocessing import MinMaxScaler
 from sktime.utils.plotting import plot_series
 from sktime.transformations.series.lag import Lag
@@ -38,15 +36,17 @@ from functions import *
 # Display versions of platforms and packages
 print('\nPython: {}'.format(platform.python_version()))
 print('Pandas: {}'.format(pd.__version__))
-print('YData-profiling: {}'.format(ydata_profiling.__version__))
 print('Scikit-learn: {}'.format(sklearn.__version__))
-print('Sktime: {}'.format(sktime.__version__))
 print('TensorFlow: {}'.format(tf.__version__))
 print('KerasTuner: {}'.format(keras_tuner.__version__))
+print('Sktime: {}'.format(sktime.__version__))
 
 
-# Constant
+# Constants
 SEED = 0
+BATCH_SIZE = 1
+EPOCHS = 50
+PATIENCE = 5
 
 # Set the random seed for reproducibility
 random.seed(SEED)
@@ -100,10 +100,6 @@ dataset.index = pd.PeriodIndex(dataset.index, freq='M')
 # Display head and the tail of the dataset
 print(pd.concat([dataset.head(), dataset.tail()]))
 
-# Time Series Profiling Report
-profile = ProfileReport(df=dataset, tsmode=True, title='Profiling Report')
-profile.to_file('dataset_report.html')
-
 # Display the monthly number of passengers
 fig, ax = plt.subplots(figsize=(12, 6))
 plot_series(
@@ -124,20 +120,24 @@ plt.show()
 2. Feature Engineering
 ===============================================================================
 """
-# Transformation of the dataset to create exogenous variables.
-# Creation of 12 lags (features)
-X = Lag([i for i in range(13)]).fit_transform(dataset).dropna()
-X = X.rename(columns={'lag_0__Passengers': 'Target'})
+# Transformation of the dataset to create an exogenous variable.
+# Given the number of passengers in month x,
+# the number of passengers is predicted for the following month x+1.
+X = Lag([0, 1]).fit_transform(dataset).dropna()
+X = X.rename(columns={
+    'lag_0__Passengers': 'Target',
+    'lag_1__Passengers': 'Exog'})
 
 # Display head and the tail of the dataset
 print(pd.concat([X.head(), X.tail()]))
 
-# Display the target
+# Display the target and exogenous variable
 fig, ax = plt.subplots(figsize=(12, 6))
 plot_series(
     X.Target,
-    labels=['Target'],
-    markers=['.'],
+    X.Exog,
+    labels=['Target', 'Exogenous variable'],
+    markers=['.', '.'],
     x_label='Date',
     y_label='Passengers',
     ax=ax)
@@ -147,51 +147,47 @@ ax.grid(True)
 plt.show()
 
 # Split the dataset into training and test sets
-train_set = X.iloc[0:int(0.8 * X.shape[0]),]
-test_set = X.iloc[train_set.shape[0]:X.shape[0],]
+train = X.iloc[0:int(0.8 * X.shape[0]),]
+test = X.iloc[train.shape[0]:X.shape[0],]
 
 # Display head and the tail of the datasets
-print(f'\n\nTraining set shape: {train_set.shape}')
-print(pd.concat([train_set.head(), train_set.tail()]))
-print(f'\nTest set shape: {test_set.shape}')
-print(pd.concat([test_set.head(), test_set.tail()]))
+print(f'\nTraining set shape: {train.shape}')
+print(pd.concat([train.head(), train.tail()]))
+print(f'\nTest set shape: {test.shape}')
+print(pd.concat([test.head(), test.tail()]))
 
 # Display training and test targets
 fig, ax = plt.subplots(figsize=(12, 6))
 plot_series(
-    train_set.Target,
-    test_set.Target,
+    train.Target,
+    test.Target,
     labels=['Training Target', 'Test Target'],
     markers=['.', '.'],
     x_label='Date',
     y_label='Passengers',
     ax=ax)
 ax.set_title(f'Monthly number of passengers from '
-             f'{train_set.index.min()} to {test_set.index.max()}')
+             f'{train.index.min()} to {test.index.max()}')
 ax.grid(True)
 plt.show()
 
 # Standardisation
 # Scale values between the range of 0 and 1
-scaler = MinMaxScaler()
-y_train = scaler.fit_transform(np.array(train_set.Target).reshape(-1, 1))
-y_test = scaler.transform(np.array(test_set.Target).reshape(-1, 1))
-
 exog_scaler = MinMaxScaler()
-X_train = exog_scaler.fit_transform(
-    np.array(train_set.drop(['Target'], axis=1)))
-X_test = exog_scaler.transform(
-    np.array(test_set.drop(['Target'], axis=1)))
+X_train = exog_scaler.fit_transform(np.array(train.Exog).reshape(-1, 1))
+X_test = exog_scaler.transform(np.array(test.Exog).reshape(-1, 1))
+
+scaler = MinMaxScaler()
+y_train = scaler.fit_transform(np.array(train.Target).reshape(-1, 1))
+y_test = scaler.transform(np.array(test.Target).reshape(-1, 1))
+
+# Reshape exogenous variable three-dimensional for the LSTM model
+X_train = X_train.reshape(X_train.shape[0], X_train.shape[1], 1)
+X_test = X_test.reshape(X_test.shape[0], X_test.shape[1], 1)
 print(f'\nX_train shape: {X_train.shape}')
 print(f'y_train shape: {y_train.shape}')
 print(f'X_test shape: {X_test.shape}')
 print(f'y_test shape: {y_test.shape}')
-
-# Reshape exogenous features three-dimensional for the LSTM model
-X_train = X_train.reshape(X_train.shape[0], X_train.shape[1], 1)
-X_test = X_test.reshape(X_test.shape[0], X_test.shape[1], 1)
-print(f'\nX_train shape: {X_train.shape}')
-print(f'X_test shape: {X_test.shape}')
 
 
 
@@ -215,14 +211,14 @@ early_stopping_callback = EarlyStopping(
     monitor='val_loss',
     mode='min',
     verbose=0,
-    patience=5)
+    patience=PATIENCE)
 
 # Train and find the best model
 tuner.search(
     x=X_train,
     y=y_train,
-    epochs=50,
-    batch_size=1,
+    epochs=EPOCHS,
+    batch_size=BATCH_SIZE,
     validation_split=0.2,
     callbacks=[early_stopping_callback])
 
@@ -235,12 +231,19 @@ print(model.summary())
 
 # Make predictions
 y_pred = model.predict(X_test)
-print(f'\ny_pred shape: {y_pred.shape}')
 
 # Evaluate the model
 scores = model.evaluate(X_test, y_test, verbose=0)
 for metric, score in zip(['Loss (MSE)', 'MSE', 'MAE'], scores):
     print(f'{metric}: {score:.3f}')
+
+# Denormalise the predicted target
+print(f'\ny_pred shape: {y_pred.shape}')
+pred_target = scaler.inverse_transform(y_pred)
+print('Predicted target shape: ', pred_target.shape)
+print('Predicted target:\n', pred_target.flatten())
+print('\nActual target shape: ', np.array(test.Target).shape)
+print('Actual target:\n', np.array(test.Target))
 
 # Display metrics for predictions
 display_tensorflow_metrics(y_test, y_pred)
@@ -249,43 +252,36 @@ display_sklearn_metrics(y_test, y_pred, SEED)
 # Display predictions
 fig, ax = plt.subplots(figsize=(8, 5))
 plot_series(
-    pd.Series(data=y_test.flatten(), index=test_set.index),
-    pd.Series(data=y_pred.flatten(), index=test_set.index),
+    pd.Series(data=y_test.flatten(), index=test.index),
+    pd.Series(data=y_pred.flatten(), index=test.index),
     labels=['Test', 'Predictions'],
     markers=['.', '.'],
     x_label='Date',
     ax=ax)
 ax.set_title(f'Actual vs. Predictions from '
-             f'{test_set.index.min()} to {test_set.index.max()}')
+             f'{test.index.min()} to {test.index.max()}')
 ax.grid(True)
 plt.show()
 
-# Denormalise the predicted target
-pred_target = scaler.inverse_transform(y_pred)
-print('\nPredicted target shape: ', pred_target.shape)
-print('Predicted target:\n', pred_target.flatten())
-print('\nActual target shape: ', np.array(test_set.Target).shape)
-print('Actual target:\n', np.array(test_set.Target))
-
 # Display metrics for actual values
 display_tensorflow_metrics(
-    np.array(test_set.Target), pred_target.flatten())
+    np.array(test.Target), pred_target.flatten())
 display_sklearn_metrics(
-    np.array(test_set.Target), pred_target.flatten(), SEED)
+    np.array(test.Target), pred_target.flatten(), SEED)
 
 # Display actual values of the training target
 # and the test target compared to predictions
 fig, ax = plt.subplots(figsize=(12, 6))
 plot_series(
-    train_set.Target,
-    test_set.Target,
-    pd.Series(data=pred_target.flatten(), index=test_set.index),
+    train.Target,
+    test.Target,
+    pd.Series(data=pred_target.flatten(), index=test.index),
     labels=['Training Target', 'Test Target', 'Predictions'],
     markers=['.', '.', '.'],
     x_label='Date',
     y_label='Passengers',
     ax=ax)
 ax.set_title(f'Training Target and Test Target vs. Predictions from '
-             f'{train_set.index.min()} to {test_set.index.max()}')
+             f'{train.index.min()} to {test.index.max()}')
 ax.grid(True)
 plt.show()
